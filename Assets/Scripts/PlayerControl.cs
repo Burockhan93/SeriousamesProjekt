1f4e014e71420c57691ceca7c5a36ad73d04cfa5
burakhan dogan <burakhandogan93@gmail.com>
2021-05-12 20:55:13 +0200

    animator verbessert

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -11,48 +12,73 @@ public class PlayerControl : MonoBehaviour
 
     private float _fruitCoolDown = 2;
     private Animator _animator;
+    public UnityEvent<float> onWalkingInput;
 
+    private void Awake()
+    {
+        onWalkingInput = new UnityEvent<float>();
+        onWalkingInput.AddListener(Walk);
+    }
+
+    private void Walk(float val)
+    {
+        _animator.SetFloat("Speed_f", val); // Wenn das Player sich bewegt, summirieren wir die Werte von beiden Axis und setzen wir die Wert ins Speed_f ein. Diese Parameter ist für die "Running Animation" verantwortlich
+        _animator.SetBool("Static_b", false); // Sollte man Static state "Static_b" false setzt, erfüllt man eine Voraussetzung für "Runinng animation". Die andere 
+    }
     private void Start()
     {
         _animator = GetComponent<Animator>();
     }
     void Update()
     {
-       // if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0) return;
-        if (Input.GetKeyDown(KeyCode.Space) && _fruitCoolDown >=2)
+        // if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0) return;
+        if (Input.GetKeyDown(KeyCode.Space) && _fruitCoolDown >= 2)
         {
             _fruitCoolDown -= Time.deltaTime;
             throwFruit();
         }
-        
+
         // Get the horizontal and vertical axis.
         // By default they are mapped to the arrow keys.
         // The value is in the range -1 to 1
         float translation = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 
-         // Make it move 10 meters per second instead of 10 meters per frame...
-         translation *= Time.deltaTime;
-         rotation *= Time.deltaTime;
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
 
-         // Move translation along the object's z-axis
-         transform.Translate(0, 0, translation);
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
 
         // Rotate around our y-axis
-        if (Input.GetAxis("Horizontal") != 0) {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
             transform.Rotate(0, rotation, 0);
         }
         else
         {
             Quaternion newRotation = Quaternion.Euler(0, camOrient.transform.eulerAngles.y, 0);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, 10* Time.deltaTime);
-            
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, 10 * Time.deltaTime);
+
         }
 
+
+        InvokeWalkingEvent();
+        
         //animationen, die werden noch verbessert. Zurzeit läuft er nur
-        _animator.SetFloat("Speed_f", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal")))); // Wenn das Player sich bewegt, summirieren wir die Werte von beiden Axis und setzen wir die Wert ins Speed_f ein. Diese Parameter ist für die "Running Animation" verantwortlich
-        _animator.SetBool("Static_b", false); // Sollte man Static state "Static_b" false setzt, erfüllt man eine Voraussetzung für "Runinng animation". Die andere ist : Speed_f >=0.5f
+        //_animator.SetFloat("Speed_f", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal")))); // Wenn das Player sich bewegt, summirieren wir die Werte von beiden Axis und setzen wir die Wert ins Speed_f ein. Diese Parameter ist für die "Running Animation" verantwortlich
+        //_animator.SetBool("Static_b", false); // Sollte man Static state "Static_b" false setzt, erfüllt man eine Voraussetzung für "Runinng animation". Die andere ist : Speed_f >=0.5f
+    }
+
+    private void InvokeWalkingEvent()
+    {
+        var walkingValue = (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        if (walkingValue < 0.5f) return;
+
+        onWalkingInput.Invoke(walkingValue);
     }
 
     void throwFruit()
